3d405ce5717dbc4f306a2d51c01190381e7c2b44
Gareth Shapiro <gareth.shapiro@tui.co.uk>
2019-05-24 09:00:15 +0100

    Upgrade to Swift 5

--- Example/SwiftEventBusExample/SwiftEventBus/SwiftEventBus.h
+++ Example/SwiftEventBusExample/SwiftEventBus/SwiftEventBus.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double SwiftEventBusVersionNumber;
 FOUNDATION_EXPORT const unsigned char SwiftEventBusVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftEventBus/PublicHeader.h>
-
-
