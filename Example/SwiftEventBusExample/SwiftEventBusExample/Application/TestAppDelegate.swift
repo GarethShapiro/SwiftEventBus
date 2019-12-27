44df98df22d6bb6d06ab79107633ff2b11fc65d0
Gareth Shapiro <gareth@sebenza-systems.com>
2019-12-27 23:12:04 +0000

    Remove window from test app delegate.

--- Example/SwiftEventBusExample/SwiftEventBusExample/Application/TestAppDelegate.swift
+++ Example/SwiftEventBusExample/SwiftEventBusExample/Application/TestAppDelegate.swift
@@ -10,7 +10,6 @@ import UIKit
 /**
 *
 * An Application Delegate which faciliates headless unit tests.
-* A plain old UIViewController is supplied as rootViewController so that none of the application code runs while the tests do.
 * See main.swift for how we get here.
 *
 **/
@@ -18,19 +17,9 @@ class TestAppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+	func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 
-		self.window = UIWindow(frame:UIScreen.main.bounds) as UIWindow?
-
-		if let window = self.window{
-
-			// Simply add a stub UIViewController to the window and go.
-			window.rootViewController = UIViewController()
-			window.rootViewController?.view.frame = window.frame
-
-			window.makeKeyAndVisible()
-
-		}
 
 		// Override point for customization after application launch.
 		return true
